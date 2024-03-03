cc2485613e7f29da52b6e95645a81b6dfba74e2e
powskierman <mlapoint99@hotmail.com>
2024-03-02 15:43:38 -1000

    Initial Commit

---
+++ FujitsuIphone/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FujitsuIphone
+//
+//  Created by Michel Lapointe on 2024-03-02.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FujitsuIphone/FujitsuIphoneApp.swift
@@ -0,0 +1,17 @@
+//
+//  FujitsuIphoneApp.swift
+//  FujitsuIphone
+//
+//  Created by Michel Lapointe on 2024-03-02.
+//
+
+import SwiftUI
+
+@main
+struct FujitsuIphoneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
